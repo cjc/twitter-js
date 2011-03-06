954653d91c9e67eb8103772a41b80a8af27b5f1c
cjc <cjc@cjc.id.au>
2011-03-06 16:09:20 +1000

    Turn test app into a spark based app, clean up the form a bit and store oauth tokens in session

--- test/client.js
+++ test/client.js
@@ -4,23 +4,27 @@
  * 127.0.0.1 twitter-js to your /etc/hosts
  */
 
+process.sparkEnv = process.sparkEnv || {};
+
 var express = require('express'),
     connect = require('connect'),
     twitterClient = require('./../')(
-      'yourKey',
-      'yourPass',
-      'http://twitter-js.com:3003/'
+      process.sparkEnv.twitterKey,
+      process.sparkEnv.twitterPass,
+      'http://localhost:3003/'
     ),
     app = express.createServer(
       connect.bodyDecoder(),
       connect.cookieDecoder(),
-      connect.session()
+connect.session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }})
     );
 
 app.set('views', __dirname);
 
 app.get('/', function (req, res) {
-  twitterClient.getAccessToken(req, res, function (error, token) {
+  twitterClient.getAccessToken(req, res, function (error, token, extras) {
+    console.log(extras);
+    console.log(token);
     res.render('client.jade', {
       layout: false,
       locals: {
@@ -31,16 +35,45 @@ app.get('/', function (req, res) {
 });
 
 app.post('/message', function (req, res) {
+  console.log(req.session);
   twitterClient.apiCall(
     'POST',
     '/statuses/update.json',
-    {token: {oauth_token_secret: req.param('oauth_token_secret'), oauth_token: req.param('oauth_token'), status: req.param('message')}},
+    {token: {oauth_token_secret: req.session.auth.newsecret, oauth_token: req.session.auth.newtoken},status: req.param('message')},
     function (error, result) {
       console.log(error);
       console.log(result);
-      res.render('done.jade', {layout: false});
+      res.render('client.jade', {layout: false});
     }
   );
 });
 
-app.listen(3003);
+app.get('/verify', function (req, res) {
+  console.log(req.session);
+  twitterClient.apiCall(
+    'GET',
+    '/account/verify_credentials.json',
+    {token: {oauth_token_secret: req.session.auth.newsecret, oauth_token: req.session.auth.newtoken}},
+    function (error, result) {
+      console.log(error);
+      console.log(result);
+      res.render('client.jade', {layout: false});
+    }
+  );
+});
+
+app.get('/image', function (req, res) {
+  console.log(req.session);
+  twitterClient.apiCall(
+    'GET',
+    '/users/profile_image/.json',
+    {token: {oauth_token_secret: req.session.auth.newsecret, oauth_token: req.session.auth.newtoken}, size:'bigger'},
+    function (error, result) {
+      console.log(error);
+      console.log(result);
+      res.render('client.jade', {layout: false});
+    }
+  );
+});
+
+module.exports = app;
